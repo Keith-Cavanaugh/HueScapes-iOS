f5ca85f537bc6dbf77e05da23ad26fec592da221
Keith-Cavanaugh <kcavanaugh3@gatech.edu>
2015-12-03 21:50:24 -0500

    Final Commits
    About page implemented for the presentation

--- AboutView.swift
+++ AboutView.swift
@@ -1,9 +1,76 @@
 //
-//  AboutView.swift
-//  Huescapes
+//  CustomViewController.swift
+//  Huescapes iOS Swift
 //
-//  Created by Keith Cavanaugh on 12/3/15.
-//  Copyright © 2015 Huescapes. All rights reserved.
+//  Created by John Wrobel on 12/1/15.
 //
 
-import Foundation
+import UIKit
+
+
+
+class AboutView : UIViewController {
+    
+    var lightTimer = NSTimer()
+    
+    override func viewDidLoad() {
+        lightTimer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: Selector("pulseColor"), userInfo: nil, repeats: true)
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        lightTimer.invalidate()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var changeDirection = 0
+    
+    @IBAction func pulseColor() {
+        let cache = PHBridgeResourcesReader.readBridgeResourcesCache()
+        let bridgeSendAPI = PHBridgeSendAPI()
+        
+        for light in cache!.lights!.values {
+            
+            // don't update state of non-reachable lights
+            if light.lightState!.reachable == 0 {
+                continue
+            }
+            
+            let lightState = PHLightState()
+            
+            lightState.transitionTime = 30
+            lightState.hue = Int(arc4random_uniform(65535))
+            lightState.saturation = 254
+            
+            if(changeDirection == 0) {
+                lightState.brightnessIncrement = 100
+            } else {
+                lightState.brightnessIncrement = -100
+            }
+            
+            // Send lightstate to light
+            bridgeSendAPI.updateLightStateForId(light.identifier, withLightState: lightState, completionHandler: { (errors: [AnyObject]!) -> () in
+                
+                if errors != nil {
+                    let message = String(format: NSLocalizedString("Errors %@", comment: ""), errors)
+                    NSLog("Response: \(message)")
+                }
+            })
+            
+        }
+        
+        if(changeDirection == 0) {
+            changeDirection = 1
+        } else {
+            changeDirection = 0
+        }
+    }
+}
